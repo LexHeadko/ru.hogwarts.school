7099dd569efb6a5f6dfa14f88b132eb0f27cd45a
ASUTP <n-odes11rus@yandex.ru>
2024-02-27 10:19:19 +0200

    create class HelloController

---
+++ src/main/java/ru/hogwarts/school/controller/HelloController.java
@@ -0,0 +1,11 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    public String hello() {
+        return "Welcome to Hogwarts!";
+    }
+}
